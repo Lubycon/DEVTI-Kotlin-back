9ebd6fcfe02aa46e7e3358f9bb4aeacb52ea2104
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-19 15:51:58 +0900

    first

---
+++ src/main/kotlin/com/lubycon/devti/DevtiKotlinBackApplication.kt
@@ -0,0 +1,11 @@
+package com.lubycon.devti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevtiKotlinBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevtiKotlinBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/lubycon/devti/DevtiKotlinBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lubycon.devti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevtiKotlinBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
