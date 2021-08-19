7d81cf27315778e221dfeb0076dab6886bd815c0
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-20 04:21:55 +0900

    clean commit

--- src/test/kotlin/com/lubycon/devti/bdd/advertisement/AdvertisementFeature.kt
+++
@@ -1,7 +0,0 @@
-package com.lubycon.devti.bdd.advertisement
-
-import org.springframework.test.context.ContextConfiguration
-
-@ContextConfiguration(classes = [SpringDataConfig::class])
-class AdvertisementFeature {
-}
