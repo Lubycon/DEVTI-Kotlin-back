67be5c40266b9499c77b68741a24ff031131f62c
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-20 17:28:57 +0900

    <feat> add Survey Entity

---
+++ src/main/kotlin/com/lubycon/devti/domain/survey/entity/Survey.kt
@@ -0,0 +1,33 @@
+package com.lubycon.devti.domain.survey.entity
+
+
+import com.lubycon.devti.global.code.SurveyType
+import com.lubycon.devti.global.code.TestType
+import com.lubycon.devti.global.entity.BaseTimeEntity
+import javax.persistence.*
+
+@Entity
+class Survey(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "survey_id")
+    private var id: Long = 0,
+
+    @Column(length = 500)
+    private var comment: String,
+
+    @Enumerated(EnumType.STRING)
+    private var surveyType: SurveyType,
+
+    @Column(length = 100, unique = true)
+    private var email: String,
+
+    @Column(length = 50, unique = true)
+    private var phone: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "test_type")
+    private var testType: TestType
+
+): BaseTimeEntity()
