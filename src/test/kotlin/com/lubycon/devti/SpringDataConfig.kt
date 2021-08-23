354e2fc68e26e9c506b64e63a3e5a28f94921b21
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-24 06:31:15 +0900

    <fix> Querydsl 테스트 환경에서 빈 주입

--- src/test/kotlin/com/lubycon/devti/SpringDataConfig.kt
+++ src/test/kotlin/com/lubycon/devti/SpringDataConfig.kt
@@ -6,16 +6,16 @@ import io.kotest.spring.SpringAutowireConstructorExtension
 import io.kotest.spring.SpringListener
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @DataJpaTest
+@Import(TestQueryDslConfig::class)
 @EntityScan(basePackages = ["com.lubycon.devti.domain"])
 @EnableJpaRepositories(basePackages = ["com.lubycon.devti.domain"])
 class SpringDataConfig : AbstractProjectConfig() {
-
     override fun listeners() = listOf(SpringListener)
 
     override fun extensions(): List<Extension> = listOf(SpringAutowireConstructorExtension)
-
-
 }
+
