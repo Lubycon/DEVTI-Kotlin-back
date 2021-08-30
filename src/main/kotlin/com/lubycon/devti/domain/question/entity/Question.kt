137d7602c9091da159fb533b85642cb82440346c
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-31 02:20:15 +0900

    <feat> add Question entity

---
+++ src/main/kotlin/com/lubycon/devti/domain/question/entity/Question.kt
@@ -0,0 +1,33 @@
+package com.lubycon.devti.domain.question.entity
+
+import com.fasterxml.jackson.annotation.JsonManagedReference
+import com.lubycon.devti.global.code.AnswerType
+import com.lubycon.devti.global.code.Pillar
+import javax.persistence.*
+
+@Entity
+class Question(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "QUESTION_ID")
+    private val id: Long = 0,
+
+
+    @Column(name = "title", length = 100)
+    private var title: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "pillar")
+    private val pillar: Pillar,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "answer_type")
+    private val answerType: AnswerType,
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "label", cascade = [CascadeType.ALL], orphanRemoval = true)
+    private val labels: Set<Preset> = HashSet<Preset>()
+
+) {
+}
