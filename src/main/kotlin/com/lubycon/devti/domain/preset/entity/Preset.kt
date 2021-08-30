cf599807de915e1cb40c7cc08a828b2ddae8b922
Hyeongwon-up <leehwdatabase@gmail.com>
2021-08-31 02:23:38 +0900

    <feat> add Preset entity

---
+++ src/main/kotlin/com/lubycon/devti/domain/preset/entity/Preset.kt
@@ -0,0 +1,32 @@
+package com.lubycon.devti.domain.preset.entity
+
+import com.fasterxml.jackson.annotation.JsonBackReference
+import com.lubycon.devti.global.code.BiasType
+import com.lubycon.devti.global.entity.BaseTimeEntity
+import javax.persistence.*
+
+@Entity
+class Preset(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "preset_id")
+    private var id: Long = 0,
+
+    @Column(name = "sequence")
+    private val sequence: Long,
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private val question: Question,
+
+    @Column(length = 100)
+    private val label: String? = null,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "bias")
+    private val bias: BiasType,
+
+    @Column(name = "weight")
+    private val weight: Float
+): BaseTimeEntity()
