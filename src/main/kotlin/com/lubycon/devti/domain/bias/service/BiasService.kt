9fd36aa7906a3e9b78ed5e9a3d70daffc4d49edc
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-01 05:11:16 +0900

    <feat> add bias service

--- src/main/kotlin/com/lubycon/devti/domain/bias/service/BiasService.kt
+++ src/main/kotlin/com/lubycon/devti/domain/bias/service/BiasService.kt
@@ -1,7 +1,10 @@
 package com.lubycon.devti.domain.bias.service
 
+import com.lubycon.devti.domain.bias.entity.Bias
+import com.lubycon.devti.global.code.BiasType
 import org.springframework.stereotype.Component
 
 @Component
 interface BiasService {
+    fun findBiasListByBiasIsNotIn(biasType: List<BiasType>): List<Bias>
 }
