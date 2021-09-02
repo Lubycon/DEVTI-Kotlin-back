1608a11a3ea99c05aeb2a1f06d84baf0663cc955
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-03 02:46:03 +0900

    <refact> annotation 추가

--- src/main/kotlin/com/lubycon/devti/domain/answer/service/AnswerService.kt
+++ src/main/kotlin/com/lubycon/devti/domain/answer/service/AnswerService.kt
@@ -2,7 +2,9 @@ package com.lubycon.devti.domain.answer.service
 
 import com.lubycon.devti.domain.answer.entity.Answer
 import com.lubycon.devti.domain.answer.entity.AnswerAttribute
+import org.springframework.stereotype.Component
 
+@Component
 interface AnswerService {
     fun createAnswer(answerAttributList: List<AnswerAttribute>): Answer
 }
--- src/main/kotlin/com/lubycon/devti/domain/answer/service/AnswerServiceImpl.kt
+++ src/main/kotlin/com/lubycon/devti/domain/answer/service/AnswerServiceImpl.kt
@@ -3,9 +3,11 @@ package com.lubycon.devti.domain.answer.service
 import com.lubycon.devti.domain.answer.dao.AnswerRepository
 import com.lubycon.devti.domain.answer.entity.Answer
 import com.lubycon.devti.domain.answer.entity.AnswerAttribute
+import org.springframework.stereotype.Service
 
+@Service
 class AnswerServiceImpl(
-    private final val answerRepository: AnswerRepository
+    private val answerRepository: AnswerRepository
 ): AnswerService {
     override fun createAnswer(answerAttributList: List<AnswerAttribute>)
         = answerRepository.save(Answer(answerList = answerAttributList))
