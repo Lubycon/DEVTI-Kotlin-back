41ae1d0b34b370ed7107b67d981e0a6eb6b3129e
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-24 04:05:57 +0900

    <test> 필러별 가중치 적용 후 계산

--- src/test/kotlin/com/lubycon/devti/domain/devti/service/DevtiAnalysiServiceTest.kt
+++ src/test/kotlin/com/lubycon/devti/domain/devti/service/DevtiAnalysiServiceTest.kt
@@ -16,6 +16,7 @@ class DevtiAnalysiServiceTest(val devtiAnalysiService: DevtiAnalysiService) : Fu
 
     private val answerList = mutableListOf<AnswerAttribute>()
     private var result = HashMap<BiasType, Int>()
+
     private val mockAnswer = MockAnswer
 
 
@@ -24,21 +25,31 @@ class DevtiAnalysiServiceTest(val devtiAnalysiService: DevtiAnalysiService) : Fu
         for (i in 1..5) {
             answerList.add(mockAnswer.role_preset_answer(i.toLong()))
         }
-        for (i in 6..10) {
+        for (i in 6..9) {
             answerList.add(mockAnswer.scale_preset_answer(i.toLong()))
         }
-        for (i in 11..15) {
+        for (i in 11..18) {
             answerList.add(mockAnswer.interest_preset_answer(i.toLong()))
         }
-        for (i in 16..20) {
+        for (i in 16..23) {
             answerList.add(mockAnswer.priority_preset_answer(i.toLong()))
         }
     }
 
     init {
+
         test("analysisAnswer") {
             result = devtiAnalysiService.analysisAnswer(answerList)
             print(result.toString())
+        }
+
+        test("initBiasWeightMap") {
+            val weightMap = devtiAnalysiService.initBiasWeightMap();
+            print(weightMap.toString())
+        }
+        test("checkPillarWeight") {
+            val tmp = devtiAnalysiService.checkPillarWeight(answerList)
+            print(tmp.toString())
         }
     }
 
