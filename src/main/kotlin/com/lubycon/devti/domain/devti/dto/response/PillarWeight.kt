571a027efb720cec819bc863e978eb83b9afc8e7
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-24 04:05:23 +0900

    <feat> pillarWeight 정보를 담은 dto

---
+++ src/main/kotlin/com/lubycon/devti/domain/devti/dto/response/PillarWeight.kt
@@ -0,0 +1,8 @@
+package com.lubycon.devti.domain.devti.dto.response
+
+data class PillarWeight(
+    var roleWeight: Int,
+    var scaleWeight: Int,
+    var interestWeight: Int,
+    var priorityWeight: Int
+)
