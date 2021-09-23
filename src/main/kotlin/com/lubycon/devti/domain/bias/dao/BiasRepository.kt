d13fe26a45a8a5584c65b89782fe9a0aad41d142
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-23 22:52:40 +0900

    <feat> repository 명시

--- src/main/kotlin/com/lubycon/devti/domain/bias/dao/BiasRepository.kt
+++ src/main/kotlin/com/lubycon/devti/domain/bias/dao/BiasRepository.kt
@@ -3,7 +3,9 @@ package com.lubycon.devti.domain.bias.dao
 import com.lubycon.devti.domain.bias.entity.Bias
 import com.lubycon.devti.global.code.BiasType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface BiasRepository: JpaRepository<Bias, Long> {
 
     fun findAllByBiasIsNotIn(biasType: List<BiasType>): List<Bias>
