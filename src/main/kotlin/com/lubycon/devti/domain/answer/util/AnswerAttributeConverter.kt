28c66fc072173807df8e5801eeff64320c2a52dd
Hyeongwon-up <leehwdatabase@gmail.com>
2021-09-27 19:25:08 +0900

    <feat> AnswerAttributeConverter 수정

--- src/main/kotlin/com/lubycon/devti/domain/answer/util/AnswerAttributeConverter.kt
+++ src/main/kotlin/com/lubycon/devti/domain/answer/util/AnswerAttributeConverter.kt
@@ -3,6 +3,8 @@ package com.lubycon.devti.domain.answer.util
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.lubycon.devti.domain.answer.entity.AnswerAttribute
 import javax.persistence.AttributeConverter
 
@@ -13,26 +15,39 @@ private var objectMapper = ObjectMapper().configure(
 
 
 class AnswerAttributeConverter: AttributeConverter<List<AnswerAttribute>, String> {
-    override fun convertToDatabaseColumn(answerAttributeList: List<AnswerAttribute>): String {
+
+    private val gson = Gson()
+
+    override fun convertToDatabaseColumn(answerAttributeList: List<AnswerAttribute>?): String {
         return try {
-            objectMapper.writeValueAsString(answerAttributeList)
+            return gson.toJson(answerAttributeList)
         } catch (e: JsonProcessingException) {
             throw IllegalArgumentException(answerAttributeList.toString())
         }
 
     }
 
-    override fun convertToEntityAttribute(dbData: String): List<AnswerAttribute> {
-        return try {
-            objectMapper.readValue<List<AnswerAttribute>>(
-                dbData,
-                objectMapper.getTypeFactory().constructCollectionType(
-                    MutableList::class.java,
-                    AnswerAttribute::class.java
-                )
-            )
-        } catch (e: JsonProcessingException) {
-            throw IllegalArgumentException(dbData)
-        }
+//    override fun convertToEntityAttribute(dbData: String?): List<AnswerAttribute> {
+//        return try {
+//            objectMapper.readValue(
+//                dbData,
+//                objectMapper.typeFactory.constructCollectionType(
+//                    MutableList::class.java,
+//                    AnswerAttribute::class.java
+//                )
+//            )
+//        } catch (e: JsonProcessingException) {
+//            throw IllegalArgumentException(dbData)
+//        }
+//    }
+
+    override fun convertToEntityAttribute(dbData: String?): MutableList<AnswerAttribute>? {
+        if (dbData == null)
+            return null
+
+        val collectionType = object : TypeToken<MutableList<AnswerAttribute>>() {}.type
+
+        return gson.fromJson(dbData, collectionType)
     }
+
 }
