ade1ce99ccda17bec39991fcb53621356a250df5
Hyeongwon-up <leehwdatabase@gmail.com>
2021-12-22 21:35:55 +0900

    feat: 필러 순서 변경

--- src/main/kotlin/com/lubycon/devti/global/code/Pillar.kt
+++ src/main/kotlin/com/lubycon/devti/global/code/Pillar.kt
@@ -8,8 +8,8 @@ enum class Pillar(
 ) {
 
     ROLE("직군", listOf(BiasType.V, BiasType.A)),
-    SCALE("규모", listOf(BiasType.S, BiasType.C)),
     INTEREST("관심사", listOf(BiasType.P, BiasType.T)),
+    SCALE("규모", listOf(BiasType.S, BiasType.C)),
     PRIORITY("우선순위", listOf(BiasType.W, BiasType.L)),
     REFERENCE("참고정보", listOf(BiasType.J, BiasType.Y));
 
