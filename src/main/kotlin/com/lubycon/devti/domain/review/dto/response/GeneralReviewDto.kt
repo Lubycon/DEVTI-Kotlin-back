64b40a50ac390f944770f25f56663ae3f9f108dc
Hyeongwon-up <leehwdatabase@gmail.com>
2021-12-22 21:38:41 +0900

    feat: dto 구조 변경

--- src/main/kotlin/com/lubycon/devti/domain/review/dto/response/GeneralReviewDto.kt
+++ src/main/kotlin/com/lubycon/devti/domain/review/dto/response/GeneralReviewDto.kt
@@ -1,7 +1,7 @@
 package com.lubycon.devti.domain.review.dto.response
 
 data class GeneralReviewDto(
-
+    val result: String,
     val title : String,
     val content : String,
     val summaryReview: List<String>
