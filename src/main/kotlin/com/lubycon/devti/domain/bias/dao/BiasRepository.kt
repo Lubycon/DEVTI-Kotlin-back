92d2391371b1d7f24084aa02b52f5d917d67a29b
Hyeongwon-up <leehwdatabase@gmail.com>
2022-01-16 20:52:29 +0900

    feat: KrBias 찾기 추가

--- src/main/kotlin/com/lubycon/devti/domain/bias/dao/BiasRepository.kt
+++ src/main/kotlin/com/lubycon/devti/domain/bias/dao/BiasRepository.kt
@@ -3,10 +3,16 @@ package com.lubycon.devti.domain.bias.dao
 import com.lubycon.devti.domain.bias.entity.Bias
 import com.lubycon.devti.global.code.BiasType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface BiasRepository: JpaRepository<Bias, Long> {
 
     fun findAllByBiasIsNotIn(biasType: List<BiasType>): List<Bias>
+
+    @Query("SELECT b.krBias FROM Bias b where b.bias = :targetBias")
+    fun findKrBiasByBias(@Param("targetBias") targetBias: String): String
+
 }
