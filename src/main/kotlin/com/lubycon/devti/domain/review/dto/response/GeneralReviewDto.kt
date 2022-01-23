9d6be84f1c6b010d78d8802793273f1bd0f432a3
Hyeongwon-up <leehwdatabase@gmail.com>
2022-01-23 16:29:39 +0900

    refactor: job 제거

--- src/main/kotlin/com/lubycon/devti/domain/review/dto/response/GeneralReviewDto.kt
+++ src/main/kotlin/com/lubycon/devti/domain/review/dto/response/GeneralReviewDto.kt
@@ -3,7 +3,6 @@ package com.lubycon.devti.domain.review.dto.response
 data class GeneralReviewDto(
     val result: String,
     val title: String,
-    val job: String,
     val content: String,
     val summaryReview: List<String>
 )
