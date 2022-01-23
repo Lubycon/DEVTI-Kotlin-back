9e50a7104093a6378c94cb7b34a1811e72119540
Hyeongwon-up <leehwdatabase@gmail.com>
2022-01-23 16:28:50 +0900

    refactor: key 제거

--- src/main/kotlin/com/lubycon/devti/domain/preset/dto/response/PresetResDto.kt
+++ src/main/kotlin/com/lubycon/devti/domain/preset/dto/response/PresetResDto.kt
@@ -3,7 +3,6 @@ package com.lubycon.devti.domain.preset.dto.response
 import com.lubycon.devti.global.code.BiasType
 
 data class PresetResDto(
-    val key: Long,
     val label: String?,
     val bias: BiasType,
     val weight: Float,
