4ea325d2e5d7b57b049396087319da6d7bc2678d
Hyeongwon-up <leehwdatabase@gmail.com>
2022-01-31 15:27:24 +0900

    chore: 의미 없는 공백 제거

--- src/main/kotlin/com/lubycon/devti/domain/review/dto/response/GeneralReviewDto.kt
+++ src/main/kotlin/com/lubycon/devti/domain/review/dto/response/GeneralReviewDto.kt
@@ -3,6 +3,6 @@ package com.lubycon.devti.domain.review.dto.response
 data class GeneralReviewDto(
     val result: String,
     val title: String,
-    val content: String,
-    val summaryReview: List<String>
+    val summary: String,
+    val summaryList: List<String>
 )
