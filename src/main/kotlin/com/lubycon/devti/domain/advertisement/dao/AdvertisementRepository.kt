084581331b53e309360d1f5de938b2f1d9bae248
Hyeongwon-up <leehwdatabase@gmail.com>
2022-02-20 22:48:06 +0900

    refactor: 기존 광고 삭제

--- src/main/kotlin/com/lubycon/devti/domain/advertisement/dao/AdvertisementRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.lubycon.devti.domain.advertisement.dao
-
-import com.lubycon.devti.domain.advertisement.entity.Advertisement
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AdvertisementRepository : JpaRepository<Advertisement, Long>, AdvertisementRepositorySupport {
-}
